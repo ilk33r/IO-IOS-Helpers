56e3759ca80252b24987b877ba042a909aaff582
ilker özcan <lkr_051@hotmail.com>
2016-02-03 22:46:44 +0200

    Initial Commit

---
+++ ioshelpers/ioshelpers.h
@@ -0,0 +1,19 @@
+//
+//  ioshelpers.h
+//  ioshelpers
+//
+//  Created by ilker özcan on 03/02/16.
+//  Copyright © 2016 ilkerozcan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ioshelpers.
+FOUNDATION_EXPORT double ioshelpersVersionNumber;
+
+//! Project version string for ioshelpers.
+FOUNDATION_EXPORT const unsigned char ioshelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ioshelpers/PublicHeader.h>
+
+
